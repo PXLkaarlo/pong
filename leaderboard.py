82449a3a199cdf0537ab05413d6664007c3cd24b
PXLkaarlo <victor.l.immo@gmail.com>
2025-01-30 16:36:13 +0100

    leaderboard playground

--- leaderboard.py
+++ leaderboard.py
@@ -3,4 +3,14 @@ for i in range(5):
     mylist.append(int(input()))
 mylist.sort(reverse=True)
 print(mylist)
-print(mylist[0])
+print(mylist[2])
+
+necrotics = 3
+parasitics = "headcrab"
+
+hazard = (necrotics,parasitics)
+
+otherlist = [(1,"omega"), (2,"lambda"), (5,"hazard"), (4,"contanment")]
+otherlist.append(hazard)
+otherlist.sort()
+print(otherlist)
