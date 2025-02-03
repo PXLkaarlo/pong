c01a4d5f17d1df9f47c4bd8065d1611ecb0e7578
PXLkaarlo <victor.l.immo@gmail.com>
2025-02-03 14:42:24 +0100

    class demonstraion

--- leaderboard.py
+++ leaderboard.py
@@ -14,3 +14,14 @@ otherlist = [(1,"omega"), (2,"lambda"), (5,"hazard"), (4,"contanment")]
 otherlist.append(hazard)
 otherlist.sort()
 print(otherlist)
+
+class item :
+    def __init__(self, xp, lv):
+        self.xp = xp
+        self.lv = lv
+
+apple = item(20, 1)
+orange = item(50, 2)
+
+print("Apples give you", apple.xp, "xp.")
+print("But oranges get you to level", orange.lv)
