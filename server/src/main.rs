1731369a9c40f8b1f2447e03adfe3112c6f22771
aSwedishGamer <davidcarbeskar@proton.me>
2025-02-12 15:27:07 +0100

    Actually finished server

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), std::io::Error> {
     let responder = zmq::Context::new().socket(zmq::REP).unwrap();
     responder.bind("tcp://*:696969")?;
 
-    let mut leaderboard: Vec<Player> = Vec::new();
+    let mut leaderboard: Vec<Player> = Vec::with_capacity(6);
     for _ in 0..5 {
         leaderboard.push(Player {
             username: String::from("none"),
@@ -20,8 +20,7 @@ fn main() -> Result<(), std::io::Error> {
         if msg.as_str() == Some("get") {
             let mut msg = String::new();
             for item in &leaderboard {
-                msg =
-                    msg + &item.username + " " + &item.score.to_string() + " ";
+                msg = msg + &item.username + " " + &item.score.to_string() + " ";
             }
             responder.send(&msg, 0).unwrap();
         } else {
@@ -30,7 +29,7 @@ fn main() -> Result<(), std::io::Error> {
                 username: String::from(msg[0]),
                 score: msg[1].parse().unwrap(),
             });
-            leaderboard.sort_by_key(|x| x.score);
+            leaderboard.sort_by_key(|x| std::cmp::Reverse(x.score));
             if leaderboard.len() < 5 {
                 leaderboard.swap_remove(5);
             }
